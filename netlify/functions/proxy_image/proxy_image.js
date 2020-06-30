4e1cf03abea79f577de421c7d6f4320325fffc50
umatoma <umatomakun@gmail.com>
2020-07-01 00:57:50 +0900

    Netlify image proxy

---
+++ netlify/functions/proxy_image/proxy_image.js
@@ -0,0 +1,25 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event, context) => {
+    try {
+        const url = event.queryStringParameters.url;
+        const res = await fetch(url);
+        const buffer = await res.buffer();
+        const base64 = buffer.toString('base64');
+        const headers = {
+            'content-type': res.headers.get('content-type'),
+            'content-length': res.headers.get('content-length'),
+        };
+        return {
+            statusCode: 200,
+            headers: headers,
+            isBase64Encoded: true,
+            body: base64,
+        };
+    } catch (e) {
+        return {
+            statusCode: 500,
+            body: e.message,
+        };
+    }
+}
